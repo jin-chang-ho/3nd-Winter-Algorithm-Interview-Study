ee4aa41378802e5ecbda01d2bcf3487ecee6a165
jin-chang-ho <ckdgh8204@naver.com>
2022-01-10 02:15:51 +0900

    Time: 51 ms (21.63%), Space: 14.4 MB (19.95%) - LeetHub

--- 1일차/819-most-common-word/819-most-common-word-answer.py
+++ 1일차/819-most-common-word/819-most-common-word-answer.py
@@ -1,10 +1,11 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
-            .lower().split() if word not in banned]
+            .lower().split() if word not in banned] # 해당 정규식은 단어 문자를 제외한 값은 ' '로 치환한다는 뜻
 
         counts = collections.Counter(words)
 
+        # 흔하게 등장하는 단어와 그 갯수를 출력하는 dictionary.most_common() 함수 -> 출력 형태는 [()]
         return counts.most_common(1)[0][0]
         
         
--- 1일차/819-most-common-word/819-most-common-word.py
+++ 1일차/819-most-common-word/819-most-common-word.py
@@ -4,17 +4,17 @@ class Solution:
         par_list = paragraph.split()
 
         for i in range(len(par_list)):
-            if par_list[i].isalpha():
+            if par_list[i].isalpha(): # 문자열이 알파벳인지 확인하는 str.isalpha() 함수
                 par_list[i] = par_list[i].lower()
             else:
                 par_list[i] = [al for al in par_list[i] if al.isalpha()]  
                 par_list[i] = "".join(par_list[i]).lower()
                 
-        par_list = [par for par in par_list if par not in banned]
+        par_list = [par for par in par_list if par not in banned] # 특정 문자열이 리스트에 있는지 확인하는 in
         
-        count = Counter(par_list)
+        count = Counter(par_list) # 각 리스트 요소를 키로, 해당 요소의 갯수를 값으로 저장하는 Counter dictionary
         
-        for key, value in count.items():
+        for key, value in count.items(): # 딕셔너리의 키, 값을 반환하는 dictionary.items() 함수
             if value == max(count.values()):
                 return key
         
