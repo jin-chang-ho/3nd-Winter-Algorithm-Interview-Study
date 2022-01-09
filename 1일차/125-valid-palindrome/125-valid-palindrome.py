cf2c064a1a7b10d96f8ed4e8c18c25d064cdc39b
jin-chang-ho <ckdgh8204@naver.com>
2022-01-09 21:06:56 +0900

    Time: 56 ms (38.67%), Space: 16.1 MB (15.30%) - LeetHub

--- 1일차/125-valid-palindrome/125-valid-palindrome.py
+++ 1일차/125-valid-palindrome/125-valid-palindrome.py
@@ -4,11 +4,11 @@ class Solution:
         
         for char in s:
             if char >= 'a' and char <= 'z':
-                correct_list.append(i)
+                correct_list.append(char)
             if char >= 'A' and char <= 'Z':
-                correct_list.append(i.lower()) # 대문자를 소문자로 바꿔주는 string.lower() 함수
+                correct_list.append(char.lower()) # 대문자를 소문자로 바꿔주는 string.lower() 함수
             if char.isdigit():
-                correct_list.append(i)
+                correct_list.append(char)
         
         # 리스트의 요소를 역순으로 바꿔서 반환하는 reversed 함수
         reverse_list = list(reversed(correct_list)) 
