2c758cf34d777a389905842499acdacdad5c38c0
jin-chang-ho <ckdgh8204@naver.com>
2022-01-10 02:19:03 +0900

    Time: 59 ms (14.48%), Space: 14.1 MB (96.74%) - LeetHub

--- 1일차/937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 1일차/937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -4,11 +4,12 @@ class Solution:
         num_logs_list = []
         
         for log in logs:
-            if log.split()[1].isdigit():
+            if log.split()[1].isdigit(): # 문자열이 숫자인지 확인하는 str.isdigit() 함수
                 num_logs_list.append(log)
             else:
                 alpha_logs_list.append(log)
-                
+
+        # 리스트를 정렬하는 list.sort() 함수 -> key 값은 lambda 함수로 지정가능
         alpha_logs_list.sort(key = lambda x : (x.split()[1:], x.split()[0]))
         
-        return alpha_logs_list + num_logs_list
+        return alpha_logs_list + num_logs_list # 리스트끼리 합치면 확장
