5bb97ddd1f58f8073ec0acd7acdc362544212a45
jin-chang-ho <65750442+jin-chang-ho@users.noreply.github.com>
2022-03-20 15:00:49 +0900

    Time: 2932 ms (5.01%), Space: 16 MB (47.79%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def check(index, path, al):
+            if len(path) == k:
+                result.append(path[:])
+                return
+            
+            prev_al = al[:]
+            for i in range(len(al)):
+                if path and path[-1] > al[i]:
+                    continue
+                
+                path.append(al[i])
+                al.remove(al[i])
+                index = index + 1
+                check(index, path, al)
+                al = prev_al[:]
+                path.pop()
+            
+        
+        
+        result = []
+        check(0, [], [c for c in range(1, n + 1)])
+        return result
+            
+        
