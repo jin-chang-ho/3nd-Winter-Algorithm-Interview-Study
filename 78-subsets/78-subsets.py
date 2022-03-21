77c72ca7d02cf5e47c81fa6be8375839b40c1f86
jin-chang-ho <65750442+jin-chang-ho@users.noreply.github.com>
2022-03-21 15:45:20 +0900

    Time: 56 ms (32.18%), Space: 14.1 MB (82.18%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def check(index, path):
+            for i in range(index, len(nums)):
+                if nums[i] in path:
+                    continue
+                path.append(nums[i])
+                check(i, path)
+                answer.append(path[:])
+                path.pop()
+            return
+        
+        answer = [[]]
+        check(0, [])
+        return answer
