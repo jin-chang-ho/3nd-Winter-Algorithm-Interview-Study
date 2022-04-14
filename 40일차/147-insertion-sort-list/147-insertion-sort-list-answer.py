c8e8b11512a244b9ae5516224fae411139ab8ea2
jin-chang-ho <ckdgh8204@naver.com>
2022-04-14 11:52:50 +0900

    Change

--- 40일차/147-insertion-sort-list/147-insertion-sort-list-answer.py
+++ 40일차/147-insertion-sort-list/147-insertion-sort-list-answer.py
@@ -5,13 +5,14 @@
 #         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur = parent = ListNode(None)
+        cur = parent = ListNode(0)
         while head:
             while cur.next and cur.next.val < head.val:
                 cur = cur.next
             
             cur.next, head.next, head = head, cur.next, head.next
+
+            if head and cur.val > head.val:
+                cur = parent
             
-            cur = parent
-            
-        return cur.next
+        return parent.next
