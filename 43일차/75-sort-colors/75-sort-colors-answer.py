f3aadaf4b89614248b08179a4a2e650a8fb87eca
jin-chang-ho <65750442+jin-chang-ho@users.noreply.github.com>
2022-04-15 16:36:31 +0900

    Update 75-sort-colors-answer.py

--- 43일차/75-sort-colors/75-sort-colors-answer.py
+++ 43일차/75-sort-colors/75-sort-colors-answer.py
@@ -3,23 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        num_list = [0, 0, 0]
+        red, white, blue = 0, 0, len(nums)
         
-        for num in nums:
-            if num == 0:
-                num_list[0] += 1
-            elif num == 1:
-                num_list[1] += 1
+        while white < blue:
+            if nums[white] < 1:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] > 1:
+                blue -= 1
+                nums[white], nums[blue] = nums[blue], nums[white]
             else:
-                num_list[2] += 1
-        
-        for i in range(len(nums)):
-            if num_list[0] != 0:
-                nums[i] = 0
-                num_list[0] -= 1
-            elif num_list[1] != 0:
-                nums[i] = 1
-                num_list[1] -= 1
-            elif num_list[2] != 0:
-                nums[i] = 2
-                num_list[2] -= 1
+                white += 1
