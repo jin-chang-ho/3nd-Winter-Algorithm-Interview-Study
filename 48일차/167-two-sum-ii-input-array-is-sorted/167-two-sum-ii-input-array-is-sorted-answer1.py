0384b58f878c756720ce19ee7b725f00d24f46ac
jin-chang-ho <ckdgh8204@naver.com>
2022-05-07 01:43:20 +0900

    Change

---
+++ 48일차/167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted-answer1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while not left == right:
+            if numbers[left] + numbers[right] < target:
+                left = left + 1
+            elif numbers[left] + numbers[right] > target:
+                right = right - 1
+            else:
+                return left + 1, right + 1
