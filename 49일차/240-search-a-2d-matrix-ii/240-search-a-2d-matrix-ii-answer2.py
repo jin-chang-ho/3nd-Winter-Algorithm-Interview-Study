50b9bf7bf8edfb341a7851972d8274ca5868647b
jin-chang-ho <ckdgh8204@naver.com>
2022-05-09 01:17:18 +0900

    Change

--- 49일차/240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii-answer2.py
+++ 49일차/240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii-answer2.py
@@ -1,9 +1,3 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in matrix:
-            if target >= row[0] and target <= row[len(row) - 1]:
-                index = bisect.bisect_left(row, target)
-                if index < len(row) and row[index] == target:
-                    return True
-        
-        return False
+        return any(target in row for row in matrix)
